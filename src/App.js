3e1e063928f7003a3242f610780c304e6482b136
Swiftly <ivecis@live.co.kr>
2020-03-28 18:41:10 +0900

    0.11 Now something exists.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Cat from "./Cat";
+
+function Cat({kinds}) {
+    return <h2>I love {kinds}.</h2>
+}
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello, Hello</h1>
-            <Cat />
+            <h1>Hello, hello</h1>
+            <Cat kinds="American short-hair"/>
+            <Cat kinds="Munchkin"/>
+            <Cat kinds="Russian Blue"/>
         </div>
     );
 }
--- src/Cat.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Cat() {
-    return (
-        <h3>I love cat.</h3>
-    );
-}
-
-export default Cat;
