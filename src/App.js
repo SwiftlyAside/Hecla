11e6d22bd092071112cf6d107adc5876c66ed18b
Swiftly <ivecis@live.co.kr>
2020-03-31 17:44:14 +0900

    0.13 Here comes cats.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 
-function Cat({kinds, image}) {
+function Cat({kinds, picture}) {
     return (
         <div>
             <h2>I love {kinds}.</h2>
-            <img src={image}/>
+            <img src={picture} alt={kinds}/>
         </div>
     )
 }
 
 const catILove = [
     {
+        id: 1,
         kinds: "American Shorthair",
-        image: "https://www.thesprucepets.com/thmb/QVUzJyAhwdxLpP8fAfzjX1Aus_0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-925319984-36b97d913d934d229d8b0d528a7da64e.jpg"
+        picture: "https://www.thesprucepets.com/thmb/QVUzJyAhwdxLpP8fAfzjX1Aus_0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-925319984-36b97d913d934d229d8b0d528a7da64e.jpg"
     },
     {
+        id: 2,
         kinds: "Munchkin",
-        image: "https://www.thesprucepets.com/thmb/uu7O05OIOe1jfe9pDvHdIiOteFk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/munchkin-cat-relaxing-in-the-garden-johannesburg-667587109-57d9bb0f5f9b5865168d616a.jpg"
+        picture: "https://www.thesprucepets.com/thmb/uu7O05OIOe1jfe9pDvHdIiOteFk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/munchkin-cat-relaxing-in-the-garden-johannesburg-667587109-57d9bb0f5f9b5865168d616a.jpg"
     },
     {
+        id: 3,
         kinds: "Russian Blue",
-        image: "https://www.thesprucepets.com/thmb/WlQeWMTYIRhhABWIsX915b2kg1o=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cute-russian-blue-cat-sitting-on-the-bed--female-cat--498731394-5c2a695746e0fb00015354ac.jpg"
+        picture: "https://www.thesprucepets.com/thmb/WlQeWMTYIRhhABWIsX915b2kg1o=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cute-russian-blue-cat-sitting-on-the-bed--female-cat--498731394-5c2a695746e0fb00015354ac.jpg"
     }
 ];
+
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello, hello</h1>
-            {catILove.map(cat => (
-                <Cat kinds={cat.kinds} image={cat.image}/>
+            {catILove.map(one => (
+                <Cat key={one.id} kinds={one.kinds} image={one.picture}/>
             ))}
         </div>
     );
