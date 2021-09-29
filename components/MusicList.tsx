bdb67f29e145436de827b0d2ce05c9618e5d0291
SwiftlyAside <ivecis@live.co.kr>
2021-09-29 17:19:55 +0900

    Fix build error

--- components/MusicList.tsx
+++ components/MusicList.tsx
@@ -11,20 +11,26 @@ const MusicList: React.FC<MusicListProps> = ({ onMusicSelect, musics }) => {
   const renderedList = musics?.map((music: { id: React.Key | null | undefined }) => {
     return <MusicItem key={music.id} music={music} onMusicSelect={onMusicSelect} />;
   });
-  return (
-    <List className="hecla-list" relaxed divided>
-      {musics ? (
-        renderedList
-      ) : (
-        <List.Item className="music-item">
-          <Image src="/favicon.ico" alt="nothing" rounded />
-          <ItemContent>
-            <Header>No results</Header>
-          </ItemContent>
-        </List.Item>
-      )}
-    </List>
-  );
+  if (musics) {
+    return (
+      <List className="hecla-list" relaxed divided>
+        {renderedList}
+      </List>
+    );
+  } else {
+    return (
+      <List className="hecla-list" relaxed divided>
+        {
+          <List.Item className="music-item">
+            <Image src="/favicon.ico" alt="nothing" rounded />
+            <ItemContent>
+              <Header>No results</Header>
+            </ItemContent>
+          </List.Item>
+        }
+      </List>
+    );
+  }
 };
 
 export default MusicList;
